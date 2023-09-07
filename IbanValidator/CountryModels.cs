f1e9f5cc970ad5187a5080fac874d2af20f62d9e
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2023-09-07 21:16:19 +0200

    feat: set minimal sdk to netcore 2.1

--- IbanValidator/CountryModels.cs
+++ IbanValidator/CountryModels.cs
@@ -1,55 +1,44 @@
-using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
-using Newtonsoft.Json;
+using System.Text.Json;
 
-namespace IbanValidator;
-
-public class CountryModels
+namespace IbanValidator
 {
-    private Dictionary<string, CountryModel> _models = new();
-
-    public void LoadModels()
+    public class CountryModels
     {
-        try
-        {
-            var jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "iban-countries.json");
+        private Dictionary<string, CountryModel> _models = new Dictionary<string, CountryModel>();
 
-            if (!File.Exists(jsonPath))
+        public void LoadModels()
+        {
+            try
             {
-                throw new FileNotFoundException("Unable to read iban-countries.json");
+                var jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "iban-countries.json");
+
+                if (!File.Exists(jsonPath))
+                {
+                    throw new FileNotFoundException("Unable to read iban-countries.json");
+                }
+
+                var jsonString = File.ReadAllText(jsonPath, Encoding.UTF8);
+
+                _models = JsonSerializer.Deserialize<Dictionary<string, CountryModel>>(jsonString);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to parse JSON", ex);
+            }
+        }
+
+        public CountryModel GetModel(string countryCode)
+        {
+            if (_models.TryGetValue(countryCode, out var model))
+            {
+                return model;
             }
 
-            var jsonString = File.ReadAllText(jsonPath, Encoding.UTF8);
-
-            _models = JsonConvert.DeserializeObject<Dictionary<string, CountryModel>>(jsonString);
-        }
-        catch (Exception ex)
-        {
-            throw new Exception("Unable to parse JSON", ex);
-        }
-    }
-
-    public bool ModelExists(string countryCode)
-    {
-        return _models.ContainsKey(countryCode);
-    }
-
-    public CountryModel GetModel(string countryCode)
-    {
-        if (_models.TryGetValue(countryCode, out var model))
-        {
-            return model;
-        }
-        else
-        {
             throw new ArgumentException($"CountryModel not found for {countryCode}");
         }
     }
-}
-
-public static class Bundle
-{
-    private static readonly Lazy<Assembly> executingAssembly = new Lazy<Assembly>(() => Assembly.GetExecutingAssembly());
-
-    public static Assembly Assets => executingAssembly.Value;
 }
--- IbanValidator/ISO7604.cs
+++ IbanValidator/ISO7604.cs
@@ -1,38 +1,40 @@
+using System;
 using System.Text.RegularExpressions;
 
-namespace IbanValidator;
-
-public abstract class ISO7064
+namespace IbanValidator
 {
-    private const string Mod9710ValidChars = "^([0-9]{1,})$";
-
-    public static int MOD97_10(string input)
+    public abstract class ISO7064
     {
-        if (!Regex.IsMatch(input, Mod9710ValidChars))
+        private const string Mod9710ValidChars = "^([0-9]{1,})$";
+
+        public static int MOD97_10(string input)
         {
-            return -1; // Equivalent to NSNotFound in Swift
-        }
-
-        var remainingInput = input;
-
-        while (true)
-        {
-            var chunkSize = Math.Min(9, remainingInput.Length);
-
-            var chunk = remainingInput.Substring(0, chunkSize);
-
-            if (int.TryParse(chunk, out var chunkInt) && chunkInt < 97 || remainingInput.Length < 3)
+            if (!Regex.IsMatch(input, Mod9710ValidChars))
             {
-                break;
+                return -1; // Equivalent to NSNotFound in Swift
             }
 
-            var remainder = chunkInt % 97;
+            var remainingInput = input;
 
-            var nextChunk = remainingInput.Substring(chunkSize);
+            while (true)
+            {
+                var chunkSize = Math.Min(9, remainingInput.Length);
 
-            remainingInput = $"{remainder}{nextChunk}";
+                var chunk = remainingInput.Substring(0, chunkSize);
+
+                if (int.TryParse(chunk, out var chunkInt) && chunkInt < 97 || remainingInput.Length < 3)
+                {
+                    break;
+                }
+
+                var remainder = chunkInt % 97;
+
+                var nextChunk = remainingInput.Substring(chunkSize);
+
+                remainingInput = $"{remainder}{nextChunk}";
+            }
+
+            return int.Parse(remainingInput);
         }
-
-        return int.Parse(remainingInput);
     }
 }
